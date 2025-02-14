1bd7292f45ac970024ab70b66a9283cea99913ca
AkselDelToro <officialdeltoro999@gmail.com>
2025-02-14 10:15:01 -0600

    Update App.js

--- ProjectWorthyRN/App.js
+++ ProjectWorthyRN/App.js
@@ -1,37 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-const GridScreen = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        
-        {/* Row 1 */}
-        <View style={styles.row}>
-          <View style={[styles.col, styles.col6, { height: 150 }]}>
-            <Text style={styles.text}> Header </Text>
-          </View>
-        </View>
-
-        {/* Row 2 */}
-        <View style={styles.row}>
-          <View style={[styles.col, styles.col4, { height: 650 }]}>
-            <Text style={styles.text}>Col 4 (Height 180)</Text>
-          </View>
-        </View>
-
-      </View>
-    </SafeAreaView>
-  );
-};
+export default function App() {
+ return (
+  <View style={styles.container}>
+   <Text>Project Worthy</Text>
+   <StatusBar style="auto" />
+  </View>
+ );
+}
 
 const styles = StyleSheet.create({
-container: {
+ container: {
   flex: 1,
   backgroundColor: "dodgerblue",
   alignItems: "center",
   justifyContent: "center",
  },
 });
-
-export default GridScreen;
