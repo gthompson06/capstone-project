49fb1700c70ae153a2c15fb33307d4d7d4fef4c5
Apoz626 <133711643+Apoz626@users.noreply.github.com>
2025-02-17 09:48:59 -0600

    2/17
    added styles.js

--- ProjectWorthyRN/App.js
+++ ProjectWorthyRN/App.js
@@ -26,5 +26,6 @@ const styles = StyleSheet.create({
   flex: 1,
   backgroundColor: "white",
  },
-});
+},
+);
 //in aksel branch
---
+++ ProjectWorthyRN/src/styles/Styles.js
@@ -0,0 +1,13 @@
+// import { Stylesheet } from "react-native";
+
+// export const CreateAccountStyles = StyleSheet.create({
+//  root: {
+//   alignItems: "center",
+//   padding: 20,
+//  },
+//  logo: {
+//   width: "70%",
+//   maxWidth: 300,
+//   maxHeight: 200,
+//  },
+// });
