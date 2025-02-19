03c72a7cff9f275a1d9447a30d759c9bcdd58b17
AkselDelToro <officialdeltoro999@gmail.com>
2025-02-19 10:08:46 -0600

    forgot password screen

--- ProjectWorthyRN/src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
+++ ProjectWorthyRN/src/screens/ForgotPasswordScreen/ForgotPasswordScreen.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 import { Text, View, SafeAreaView } from 'react-native'
+import CustomInput from '../../components/CustomInput/CustomInput'
+import CustomButton from '../../components/CustomButton/CustomButton'
 
 const ForgotPasswordScreen = () => {
+    const onEnterEmailPressed = () => {
+        console.warn("Enter email for forgot password pressed")
+    }
     return (
       <SafeAreaView>
-        <Text> ForgotPasswordScreen </Text>
+            <Text style={{fontSize: "40", paddingBottom: "30"}}>Forgot Password?</Text>
+            <CustomInput 
+                placeholder={"Email Address"}
+            />
+            <CustomButton 
+                text={"Enter"}
+                onPress={onEnterEmailPressed}
+            />      
       </SafeAreaView>
     )
 }
