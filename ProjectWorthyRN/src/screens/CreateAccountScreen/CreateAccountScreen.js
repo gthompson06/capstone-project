4ff7cf775048a372cae8a0bdf27a123f66c7c11f
AkselDelToro <officialdeltoro999@gmail.com>
2025-02-19 09:42:27 -0600

    Update CreateAccountScreen.js

--- ProjectWorthyRN/src/screens/CreateAccountScreen/CreateAccountScreen.js
+++ ProjectWorthyRN/src/screens/CreateAccountScreen/CreateAccountScreen.js
@@ -9,7 +9,7 @@ import React, { useState } from "react";
 import Logo from "../../../assets/images/TestImg.png";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import CustomButton from "../../components/CustomButton/CustomButton";
-// import CreateAccountStyles from "../../styles/Styles.js";
+import { CreateAccountStyles } from "../../styles/Styles.js";
 
 const CreateAccountScreen = () => {
  const [username, setUsername] = useState("");
