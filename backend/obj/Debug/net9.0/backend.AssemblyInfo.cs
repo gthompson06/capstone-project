c3663f7bdfc58eb6a8ef3983f001f879802cebc6
jjonesAU <147194355+jjonesAU@users.noreply.github.com>
2025-03-13 13:04:11 -0500

    minor changes

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da633f8dfe9df5e2e4902e978dad736438f0da1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da89387db217c12529525a385a05d4a60a0c83e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
