5fb4d39f735ef0432b78f715ff552906a3a3c9e6
AkselDelToro <officialdeltoro999@gmail.com>
2025-03-21 08:18:54 -0500

    hamburger

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native"; // Import this hook
+import { Ionicons } from "@expo/vector-icons";
 
-const HomeScreen = ({route}) => {
-  const { username } = route.params || {}; 
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ route }) => {
+  const navigation = useNavigation(); // Use this to get the correct navigation object
+  const { username } = route.params || {};
 
   return (
-    <SafeAreaView>
-      <Text>Welcome to the Home Screen, {username} </Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* Custom Hamburger Menu Button */}
+      <TouchableOpacity
+        style={{ marginLeft: 15, marginTop: 10, padding: 10 }}
+        onPress={() => navigation.openDrawer()} // Now this works
+      >
+        <Ionicons name="menu" size={30} color="black" />
+      </TouchableOpacity>
 
+      {/* Welcome Text */}
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 25,
+          paddingBottom: 30,
+          paddingTop: 0,
+        }}
+      >
+        Welcome {username}
+      </Text>
     </SafeAreaView>
   );
 };
 
+// Exporting HomeScreen wrapped in Stack Navigator
+export default function HomeStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
 
 
-export default HomeScreen;
+
+
+//on sign in screen edit out the backdoor
