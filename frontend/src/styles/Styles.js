32fb1cd156b943e2a23ff4a7e197b2cc7d12f7bd
Apoz626 <133711643+Apoz626@users.noreply.github.com>
2025-04-04 10:14:38 -0500

    Create Styles.js

---
+++ frontend/src/styles/Styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const CreateAccountStyles = StyleSheet.create({
+ root: {
+  alignItems: "center",
+  padding: 20,
+ },
+ logo: {
+  width: "70%",
+  maxWidth: 300,
+  maxHeight: 200,
+ },
+ button: {
+  display: "flex",
+  width: "100%",
+  justifyContent: "flex-start",
+ },
+});
+
+export const ResetPasswordStyles = StyleSheet.create({
+ root: {
+  alignItems: "center",
+  padding: 20,
+ },
+ logo: {
+  width: "70%",
+  maxWidth: 300,
+  maxHeight: 200,
+ },
+});
