5d634c2e85d2c027b1488f8e29f589c03b6771cb
Greg Thompson <115088274+gthompson06@users.noreply.github.com>
2025-04-16 22:34:12 -0500

    no change

--- backend/models/User.cs
+++
@@ -1,9 +0,0 @@
-public class User
-{
-    public required UserInfo UserInfo {get;set;}
-    public List<UserTask>? Tasks {get;set;}
-    public List<UserExpense>? Expenses {get;set;}
-    public List<UserBankAccount>? BankAccounts {get;set;}
-    public List<UserSchedule>? Schedules {get;set;}
-
-}
