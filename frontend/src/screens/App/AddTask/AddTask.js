041a65ee6136c7251f64613595dfbed3a2c8a578
AkselDelToro <115087508+AkselDelToro@users.noreply.github.com>
2025-04-18 11:40:15 -0500

    Update AddTask.js

--- frontend/src/screens/App/AddTask/AddTask.js
+++ frontend/src/screens/App/AddTask/AddTask.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
-
-const Stack = createStackNavigator();
 
 const AddTask = ({ route }) => {
   const navigation = useNavigation();
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      {/* Back Button */}
       <TouchableOpacity
         style={{ marginLeft: 15, marginTop: 10, padding: 10 }}
-        onPress={() => navigation.openDrawer()}
+        onPress={() => navigation.goBack()}
       >
-        <Ionicons name="menu" size={30} color="black" />
+        <Ionicons name="arrow-back" size={30} color="black" />
       </TouchableOpacity>
 
       <Text
@@ -32,10 +30,4 @@ const AddTask = ({ route }) => {
   );
 };
 
-export default function HomeStack() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="AddTask" component={AddTask} />
-    </Stack.Navigator>
-  );
-}
+export default AddTask;
