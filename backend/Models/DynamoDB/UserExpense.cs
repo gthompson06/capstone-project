693e567780bf92d566cab03bebcf0a0f8f44b4ac
jjonesAU <147194355+jjonesAU@users.noreply.github.com>
2025-04-22 16:36:04 -0500

    Added Missing Files

---
+++ backend/Models/DynamoDB/UserExpense.cs
@@ -0,0 +1,13 @@
+using Amazon.DynamoDBv2.DataModel;
+
+[DynamoDBTable("Expenses")]
+public class UserExpense
+{
+    [DynamoDBHashKey("UserId")] public required int UserId { get; set; }
+    [DynamoDBRangeKey("ExpenseId")] public required int ExpenseId { get; set; }
+    [DynamoDBProperty("Title")] public required string Title { get; set; }
+    [DynamoDBProperty("Frequency")] public required string Frequency { get; set; }
+    [DynamoDBProperty("Type")] public required string Type { get; set; }
+    [DynamoDBProperty("Amount")] public required decimal Amount { get; set; }
+    [DynamoDBProperty("PayDate")] public required string PayDate { get; set; }
+}
