c2c536e52b16e4c46e7379e380b89d5240ba7b40
jjonesAU <147194355+jjonesAU@users.noreply.github.com>
2025-04-27 10:00:10 -0500

    Removed some comments and unused imports

--- backend/controllers/UserController.cs
+++ backend/controllers/UserController.cs
@@ -71,16 +71,11 @@ public class UserController : ControllerBase
     [HttpPut("reset-password")]
     public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO request)
     {
-        // Console.WriteLine("UserName: " + request.UserName);
-        // Console.WriteLine("Answer: " + request.SecurityAnswer);
-        // Console.WriteLine("Password: " + request.NewPassword);
         var user = await _userService.GetUserInfoByUsername(request.UserName);
         if (user == null)
         {
             return NotFound(new { message = "User not found" });
         }
-        // Console.WriteLine("hash answer: " + user.SecurityAnswer);
-
         var isCorrectAnswer = _passwordService.Verify(user.SecurityAnswer, request.SecurityAnswer);
         if (!isCorrectAnswer)
         {
@@ -119,48 +114,6 @@ public class UserController : ControllerBase
             return Ok(response);
         }
     }
-    // [HttpGet("{email}")]
-    // public async Task<IActionResult> GetUserEmail(string email)
-    // {
-    //     var response = await _userService.GetUserEmail(email);
-
-    //     if (response == null)
-    //     {
-    //         return NotFound(new { Message = "404: User not found" });
-    //     }
-    //     else
-    //     {
-    //         return Ok(response);
-    //     }
-
-    // }
-
-    // [HttpPost("create")]
-    // public async Task<IActionResult> CreateUser([FromBody] UserInfo request)
-    // {
-    //     try
-    //     {
-    //         var queryConfig = new DynamoDBOperationConfig
-    //         {
-    //             IndexName = "UserName-index"
-    //         };
-
-    //         var users = await _dbContext.QueryAsync<UserInfo>(request.UserName, queryConfig).GetRemainingAsync();
-
-    //         var matchedUser = users.FirstOrDefault(user => user.Email == request.Email);
-
-    //         if (matchedUser != null) {
-    //             return Unauthorized(new { message = "Username or Email already exists" });
-    //         }
-
-    //         await _userService.PostUserInfo(request);
-    //         return CreatedAtAction(nameof(GetUserInfo), new { userId = UserInfo.Username }, UserInfo);
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         return StatusCode(500, new { message = "Error creating account", error = ex.Message });
-    //     }
-    // }
 
     [HttpPut("{userId}")]
     public async Task<IActionResult> UpdateUser(int userId, [FromBody] UserInfo updatedUser)
